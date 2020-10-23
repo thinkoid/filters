7329f58c991b7dc5d4e00345a0f7c508e449ebfd
Liviu Nicoara <lnicoara at thinkoid dot org>
2020-10-23 12:34:05 -0400

    LZW roundtrip test.

--- lzw_test.cc
+++ lzw_test.cc
@@ -1,32 +1,79 @@
 // -*- mode: c++; -*-
 
+#define BOOST_TEST_DYN_LINK
+#define BOOST_TEST_MODULE streams
+
 #include <cassert>
 
 #include <iostream>
 
+#include <boost/iostreams/device/array.hpp>
+#include <boost/iostreams/device/back_inserter.hpp>
 #include <boost/iostreams/device/file.hpp>
+#include <boost/iostreams/stream.hpp>
 #include <boost/iostreams/filtering_stream.hpp>
 namespace io = ::boost::iostreams;
+
+#include <boost/type_index.hpp>
+#include <boost/test/unit_test.hpp>
+namespace utf = boost::unit_test;
+
+#include <boost/test/data/test_case.hpp>
+#include <boost/test/data/monomorphic.hpp>
+namespace  data = boost::unit_test::data;
 
 #include <lzw_input_filter.hh>
 #include <lzw_output_filter.hh>
 
+#include <fmt/format.h>
+using fmt::print;
+
 #define UNUSED(x) ((void)x)
 
-int main(int argc, char **argv)
+BOOST_AUTO_TEST_SUITE(lzw_test)
+
+static std::vector< char >
+make_source(size_t n)
 {
-    if (argc != 2)
-        return 2;
+    std::vector< char > xs(n);
+    std::generate(xs.begin(), xs.end(), rand);
+    return xs;
+}
+
+BOOST_AUTO_TEST_CASE(init)
+{
+    srand(time(0));
+}
+
+BOOST_DATA_TEST_CASE(
+    roundtrip,
+    data::xrange< size_t >( (data::begin = 1000, data::end=10000, data::step=317)))
+{
+    std::vector< char > xs(make_source(sample)), ys;
+
+    {
+        io::filtering_ostream out;
+
+        out.push(ypdf::iostreams::lzw_output_filter_t());
+        out.push(::io::back_insert_device(ys));
+
+        for (const auto &x : xs)
+            out.put(x);
+    }
+
+    std::vector< char > zs;
 
     {
         io::filtering_istream in;
 
         in.push(ypdf::iostreams::lzw_input_filter_t());
-        in.push(::io::file_source(argv[1]));
+        in.push(::io::array_source(ys.data(), ys.size()));
 
-        for (int c; EOF != (c = ::io::get(in)); )
-            std::cout.put(c);
+        for (int c; EOF != (c = in.get()); )
+            zs.push_back(c);
     }
 
-    return 0;
+    BOOST_TEST(xs == zs);
 }
+
+BOOST_AUTO_TEST_SUITE_END()
